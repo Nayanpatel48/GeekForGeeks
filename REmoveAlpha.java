98b8253cef3c39460cf81764f72d765716700e68
nayan <www.xil187@gmail.com>
2024-06-19 12:49:28 +0530

    new answers added

--- REmoveAlpha.java
+++ REmoveAlpha.java
@@ -11,6 +11,14 @@ public class REmoveAlpha
     }
     public static String removeCharacters(String S)
     {
-        
+        java.lang.StringBuilder a = new java.lang.StringBuilder();
+        char[] b = S.toCharArray();
+        for (int i=0;i<S.length();i++)
+        {
+            if (b[i] == '1'||b[i] == '0'||b[i] == '2'||b[i] == '3'||b[i] == '4'||b[i] == '5'||b[i] == '6'||b[i] == '7'||b[i] == '8'||b[i] == '9')
+                a.append(b[i]);
+        }
+        String m = a.toString();
+        return m;
     }
 }
